23021e3ccc979fcfd3fbc4adf9a2b838e627afa7
Ali Tahir <Alitahirsu@gmail.com>
2025-10-26 10:57:28 -0700

    Code with error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./Components/Header";
 import Education from "./Components/Education";
 import { cvData } from "./data/cvData";
 
-const App: React.FC = () => {
+const App: React.FC ;= () => {
   return (
     <div className="max-w-3xl mx-auto p-6 text-gray-800">
       <Header />
